fcb3c0cc643e9100f68674eef81736a6f60634b7
glenqa <glendataywx@gmail.com>
2025-05-24 22:51:30 +0800

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Intro from "./components/Intro";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
